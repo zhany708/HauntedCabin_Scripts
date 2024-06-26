ae27397fa0804c9cff4e41793c9a5e20cfb74aa6
zhany708 <zhany708@mcmaster.ca>
2024-06-26 18:24:36 +0800

    创建祷告石UI脚本

---
+++ Screenplays/HellsCall/Altar/AltarHealthBarUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Introduction：
+ * Creator：Zhang Yu
+*/
+
+public class AltarHealthBarUI : BasePanel
+{
+    public Vector3 offset;                          // Offset for the health bar position relative to the player
+
+
+
+    Transform m_AltarTransform;                     //
+    RectTransform m_HealthBarRectTransform;
+    Camera m_MainCamera;
+
+
+
+
+
+    void Start()
+    {
+        m_HealthBarRectTransform = GetComponent<RectTransform>();
+        m_MainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        // Convert the player's world position to screen space
+        Vector3 screenPosition = m_MainCamera.WorldToScreenPoint(m_AltarTransform.position + offset);
+
+        // Update the health bar's position
+        m_HealthBarRectTransform.position = screenPosition;
+    }
+}
